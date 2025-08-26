0628fa6244dd08cc30b6e573396d5518442e6168
Yaeha-Park <parkyaeha@gmail.com>
2025-08-26 13:48:44 -0400

    Initial commit

---
+++ MCTS.py
@@ -0,0 +1,42 @@
+import random
+import math
+from TTT import ttt
+
+class MCTSNode:
+    def __init__(self, state: ttt, parent = None, move = None):
+        self.state = state
+        self.parent = parent
+        self.move = move
+        self.children = []
+        self.visits = 0
+        self.value = 0
+        self.valid = state.get_valid_moves()
+
+    def full(self):
+        return len(self.valid) == 0
+    
+    def grow(self):
+        
+        move = self.valid.pop()
+        new = self.state.make_move(move)
+        child = MCTSNode(new, parent = self, move = move)
+        self.children.append(child)
+        return child
+    
+    def winner(self, c_param = 1.41):
+
+        choices = []
+
+        for child in self.children:
+
+            if child.visits == 0:
+
+                score = float('inf')
+
+            else:
+
+                score = (child.value / child.visits) + c_param * math.sqrt(math.log(self.visits) / child.visits)
+
+            choices.append(score)
+
+        return self.children[choices.index(max(choices))]
---
+++ TTT.py
@@ -0,0 +1,44 @@
+
+import copy
+import random
+
+class ttt:
+    def __init__(self):
+        self.board = [' '] * 9
+        self.player = 'X'
+
+    def get_valid_moves(self):
+        valid = []
+        for i in range(9):
+            if self.board[i] == ' ':
+                valid.append[i]
+
+        random.shuffle(valid)
+        return valid
+
+    
+    def make_move(self, move):
+        new = copy.deepcopy(self)
+        new.board[move] = self.player
+        new.player = 'O' if self.player == 'X' else 'X'
+
+    def winner(self):
+        wins = [(0,1,2), (3,4,5), (6,7,8),
+                (0,3,6), (1,4,7), (2,5,8),
+                (0,4,8), (2,4,7)]
+        for a, b, c in wins:
+            if self.board[a] == self.board[b] == self.board[c] != ' ':
+                return self.board[a]
+            if ' ' not in self.board:
+                return 'Draw'
+            return None
+    
+    def is_terminal(self):
+        return self.get_winner() is not None
+    
+    def print_board(self):
+        for i in range(3):
+            print(self.board[i*3:(i+1)*3])
+        print()
+
+    
