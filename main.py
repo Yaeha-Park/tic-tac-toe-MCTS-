64d466eadc7b1af4db8b6bc7e220f4e12a35e195
D-Cassidy <declanpcassidy1@gmail.com>
2025-08-26 18:09:25 -0400

    Added ability to quit program instead of making a move

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3 
+import sys
 from TTT import ttt
 
 def main():
     run_cli_human_game()
 
 def run_cli_human_game():
+    # Game loop
     game = ttt()
     while (game.get_winner() == None):
         game.print_board()
         inp = get_user_input(game.get_valid_moves(), game.player)
         game.make_move(inp)
-
+    
+    # Print result and winner
     game.print_board()
     result = game.get_winner()
     if result == "Draw":
@@ -19,18 +22,23 @@ def run_cli_human_game():
         print(f"{game.get_winner()} is the winner!")
 
 def get_user_input(valid_moves, turn):
-    print(f"{turn}'s move. Valid moves: {valid_moves}")
+    print(f"{turn}'s move. Select a valid move: {valid_moves} or (q to quit)")
+    
+    # Get input that matches one of the valid moves (or q)
     while (True):
         try: 
             inp = input("> ")
-            if inp == 'q':
-                return ""
+            if inp.lower() == 'q':
+                break
             elif int(inp) not in valid_moves:
                 print("Please enter a move from the valid move list.")
             else: 
                 break
         except:
             print("Please enter an integer.")
+
+    if inp.lower() == 'q':
+        sys.exit()
 
     return int(inp)
 
